4ab3d91e3951d680dcb000fc923c9dab3f12e114
Malek Ajmi <ajmimalek@hotmail.fr>
2021-07-14 16:37:08 +0100

    Entête GET&POST - Création projet Test

---
+++ Data/IUserTasksRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UserTasksManager.Models;
+using static UserTasksManager.Models.Task;
+
+namespace UserTasksManager.Data
+{
+    public interface IUserTasksRepo
+    {
+        //GET
+        IEnumerable<User> GetUsers();
+        IEnumerable<Task> GetTasks();
+        IEnumerable<Task> GetUserTasks(User user);
+        IEnumerable<User> GetUsersByRole(Role role);
+        Task GetTaskByTitle(string title);
+        IEnumerable<Task> GetTasksByStatus(State state);
+        IEnumerable<Task> GetTasksBeforeEndDate(DateTime endDate);
+        //POST
+        User AddUser(User user);
+        Task AddTask(Task task);
+        IEnumerable<Task> AddTasksToUser(User user, IEnumerable<Task> tasks);
+        IEnumerable<Task> AddTasksToUser(string username, IEnumerable<Task> tasks);
+        //PUT
+        //PATCH
+    }
+}
---
+++ Data/UserTasksRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserTasksManager.Models;
+
+namespace UserTasksManager.Data
+{
+    public class UserTasksRepo : IUserTasksRepo
+    {
+        
+    }
+}
